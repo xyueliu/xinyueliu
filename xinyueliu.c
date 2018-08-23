60df1eaf63f7fbcea29cec152b60df5f96fd5ed3
aicoast <35319703+aicoast@users.noreply.github.com>
2018-08-23 04:13:27 -0500

    hack1.0

---
+++ xinyueliu.c
@@ -0,0 +1,16 @@
+/**
+ * Auther: Xinyue Liu
+ * Date: 2018/08/23
+ *
+ * hack 1.0
+ *
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+ 
+ int main(int argc, char **argv) {
+ 
+   printf("Xinyue Liu Computer Science");
+   
+   return 0;
+ }
