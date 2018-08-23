4d61849d84939654b967befd22f4c9af34d57e45
xyueliu <42636341+xyueliu@users.noreply.github.com>
2018-08-23 09:42:17 -0500

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Xinyue Liu and Nathan Grof
+ * Date: 2018/8/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Xinyue Liu and Nathan Grof\n");
+
+  return 0;
+}
